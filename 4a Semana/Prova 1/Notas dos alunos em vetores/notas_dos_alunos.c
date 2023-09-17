4f839ae1128e6ad3f1e32aea7f5cc5555443018f
Dio <vinilimasampaio@gmail.com>
2023-09-17 16:26:20 -0300

    Update notas_dos_alunos.c

--- 4a Semana/Prova 1/Notas dos alunos em vetores/notas_dos_alunos.c
+++ 4a Semana/Prova 1/Notas dos alunos em vetores/notas_dos_alunos.c
@@ -1,32 +1,63 @@
 #include <stdio.h>
 
-int main() {
-    int alunos, indice = 0;
-    scanf("%d", &alunos);
-    float lista[2][alunos + 1];
-
-    //Entrada
-    for(int i = 1; i <= alunos; i++) {
-        indice++;
-        lista[0][i] = indice;
-    } for(int i = 1; i <= alunos; i++) {
-        printf("%d - ", i);
-        scanf("%f", &lista[1][i]);
-
+void entrada(float vetor[], int tamanho) {
+    int descarte1;
+    char descarte2;
+    
+    for(int i = 0; i < tamanho; i++) {
+        scanf("%d %c %f", &descarte1, &descarte2, &vetor[i]);
     }
+}
 
-    //Aprovados
+int main() {
+    int alunos;
+    scanf("%d", &alunos);
+    float lista[alunos];
+
+    // Entrada
+    entrada(lista, alunos);
+
+    // Aprovados
     printf("Aprovados: ");
-    int aprovados = 0;
-    for(int i = 1; i <= alunos; i++) {
-        if(lista[1][i] >= 7){
+    int aprovados = 0; 
+
+    for(int i = 0; i < alunos; i++) {
+        if(lista[i] >= 7){
             aprovados++;
             if(aprovados > 1) {
-                printf(", ");
+                printf(", "); 
             }
-            printf("%d (%.1f)", lista[0][i], lista[1][i]);
+            printf("%d (%.1f)", i+1, lista[i]);
         }
     } printf("\n");
+
+    //Recuperação
+    printf("Recuperação: ");
+    int recuperacao = 0;
+
+    for(int i = 0; i < alunos; i++) {
+        if(lista[i] >= 5 && lista[i] < 7){
+            recuperacao++;
+            if(recuperacao > 1) {
+                printf(", ");
+            }
+            printf("%d (%.1f)", i+1, lista[i]);
+        }
+    } printf("\n");
+
+    //Reprovados
+    printf("Reprovados: ");
+    int reprovados = 0;
+
+    for(int i = 0; i < alunos; i++) {
+        if(lista[i] < 5) {
+            reprovados++;
+            if(reprovados > 1){
+                printf(", ");
+            }
+            printf("%d (%.1f)", i+1, lista[i]);
+        }
+    }
     
     return 0;
 }
