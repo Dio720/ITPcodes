74be87ab58ece112a4038bd3ae6d164797c1dcdd
Dio <vinilimasampaio@gmail.com>
2023-09-17 14:21:22 -0300

    Maior valor duplicado - Lista 1

---
+++ 4a Semana/Lista 1/Maior valor duplicado/valor_duplicado.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    bool primeiro = true;
+    int tam, maior;
+    scanf("%d", &tam);
+    int vetor[tam];
+
+    for(int i = 0; i < tam; i++) {
+        scanf("%d", &vetor[i]);
+    }
+    for(int i = 0; i < tam; i++) {
+        for(int j = 0; j < tam; j++) {
+            if(i != j) {
+                if(vetor[i] == vetor[j]) {
+                    if(pimeiro) {
+                        maior = vetor[i];
+                        primeiro = false;
+                    } else if(vetor[i] > maior) {
+                        maior = vetor[i];
+                    }
+                }
+            }
+        }
+    }
+
+    printf("%d", maior);
+
+    return 0;
+}
