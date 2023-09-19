168e0a1516ea6e0b4b70ada95a76ed2ff45a294c
Dio <vinilimasampaio@gmail.com>
2023-09-19 00:18:01 -0300

    CHUPA PORRRA CONSEGUI

--- 4a Semana/Prova 1/Bola de Ouro/bola_de_ouro.c
+++ 4a Semana/Prova 1/Bola de Ouro/bola_de_ouro.c
@@ -1,43 +1,69 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
-    int n;
-    scanf("%d", &n);
-    float bolao[3][n], indice = 1, ganhador, comes_bebes = 0, total = 0;
-    
-    for(int i = 0; i < n; i++){
-        bolao[0][i] = indice;
+int main() {
+    int N, qntdW = 0, qntdL = 0;
+    float matriz[3][30] = {0}, indice = 1, total = 0, comes_bebes = 0, ganhador;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%f %f", &matriz[1][i], &matriz[2][i]);
+        matriz[0][i] = indice;
         indice++;
+        total += matriz[1][i];
+        matriz[1][i] = floor(matriz[1][i] * 0.9);
     }
-    for(int i = 0; i < n; i++){
-        scanf("%f %f", &bolao[1][i], &bolao[2][i]);
-        total += bolao[1][i];
-        bolao[1][i] = ceil(bolao[1][i] * 0.9);
-        comes_bebes += ceil(bolao[1][i] * 0.1);
+    scanf("%f", &ganhador);
 
-    } scanf("%f", &ganhador);
+    comes_bebes = ceil(total * 0.1);
 
-
-    int vencedores = 0, perdedores = 0;
-    for(int i = 0; i < n; i++){
-        if(bolao[0][i] == ganhador){
-            vencedores++;
+    for (int i = 0; i < N; i++) {
+        if (matriz[2][i] == ganhador) {
+            qntdW++;
         } else {
-            perdedores++;
+            qntdL++;
         }
     }
-        
 
-    float mWin[2][vencedores], mLoser[2][perdedores];
-    
-    printf("Total: %0.f \n", total);
-    if(perdedores == n){
-        for(int i = 0; i < n; i++){
-            printf("Apostador %.0f: %0.f \n", bolao[0][i], bolao[1][i]);
+    float matW[2][30] = {0}, matL[2][30] = {0};
+    int indexW = 0, indexL = 0; // Índices para as matrizes matW e matL
+
+    for (int i = 0; i < N; i++) {
+        if (matriz[2][i] == ganhador) {
+            matW[0][indexW] = matriz[0][i];
+            matW[1][indexW] = matriz[1][i];
+            indexW++;
+        } else {
+            matL[0][indexL] = matriz[0][i];
+            matL[1][indexL] = matriz[1][i];
+            indexL++;
         }
     }
-    printf("Comidas e bebidas: %0.f", comes_bebes);
+
+    float sumW = 0, sumL = 0;
+
+    for (int i = 0; i < qntdW; i++) {
+        sumW += matW[1][i];
+    }
+    for (int i = 0; i < qntdL; i++) {
+        sumL += matL[1][i];
+    }
+
+    for (int i = 0; i < qntdW; i++) {
+        matW[1][i] = matW[1][i] + ((matW[1][i] / sumW) * sumL);
+    }
+
+    printf("Total: R$ %.0f \n", total);
+    if (qntdL == N) {
+        for (int i = 0; i < N; i++) {
+            printf("Apostador %.0f: R$ %.0f \n", matriz[0][i], matriz[1][i]);
+        }
+    } else {
+        for (int i = 0; i < qntdW; i++) {
+            printf("Apostador %.0f: R$ %.0f \n", matW[0][i], matW[1][i]);
+        }
+    }
+    printf("Bebidas e petiscos: R$ %.0f", comes_bebes);
 
     return 0;
 }
