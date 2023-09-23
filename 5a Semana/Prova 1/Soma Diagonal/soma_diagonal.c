a4bb0463e6adb90a7b84a4dc6d6a5efb4be28377
Dio <vinilimasampaio@gmail.com>
2023-09-23 13:59:36 -0300

    Soma Diagonal

---
+++ 5a Semana/Prova 1/Soma Diagonal/soma_diagonal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int soma_diagonal(int matriz[10][10], int n){
+    int soma_diagonal = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(i == j){
+                soma_diagonal += matriz[i][j];
+            }
+        }
+    }
+    return soma_diagonal;
+}
+
+int main(){
+    int matriz[10][10];
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+    int resultado = soma_diagonal(matriz, n);
+    printf("%d", resultado);
+
+    return 0;
+}
